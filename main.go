ce985b759e0a6460493feb8203dcab41196e53bf
wt3022 <87685849+wt3022@users.noreply.github.com>
2024-12-29 02:34:07 +0900

    [update] 必要APIの仮置き

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/wt3022/github-release-notifier/handlers"
@@ -10,6 +11,10 @@ import (
 	"github.com/wt3022/github-release-notifier/internal/env"
 	"github.com/wt3022/github-release-notifier/internal/github"
 )
+
+func todo(c *gin.Context) {
+	c.String(http.StatusOK, "todo")
+}
 
 func main() {
 	config := env.LoadConfig()
@@ -21,9 +26,41 @@ func main() {
 	}
 
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
+	router.GET("/test", func(c *gin.Context) {
 		handlers.HomeHandler(c, dbClient, githubClient)
 	})
+
+	/* TODO: ユーザー周りのAPI定義 */
+
+	/* プロジェクト */
+	projectRouter := router.Group("/projects")
+	projectRouter.GET("/:id", todo)
+	projectRouter.GET("/", todo)
+	projectRouter.POST("/", todo)
+	projectRouter.PATCH("/", todo)
+	projectRouter.DELETE("/", todo)
+
+	/* 監視リポジトリ */
+	repositoriesRouter := router.Group("/repositories")
+	repositoriesRouter.GET("/:id", todo)
+	repositoriesRouter.GET("/", todo)
+	repositoriesRouter.POST("/", todo)
+	repositoriesRouter.DELETE("/", todo)
+
+	/* 通知 */
+	notificationRouter := router.Group("/notifications")
+	notificationRouter.GET("/:id", todo)
+	notificationRouter.GET("/", todo)
+	notificationRouter.POST("/:id/test_notification", todo)
+	notificationRouter.POST("/", todo)
+	notificationRouter.PATCH("/", todo)
+	notificationRouter.DELETE("/", todo)
+
+
+	/* 定期タスク実行
+	* リポジトリの更新通知
+	*/
+
 
 	router.Run()
 }
